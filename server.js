bab6fc90d9c0dd535ffb2c042a6f3feb0ce63d4f
mochaouch <mohamed.chaouch@redlean.io>
2018-01-02 16:22:54 +0100

    fourth

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// ForceSSL middleware
-app.use(forceSSL());
+c
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
